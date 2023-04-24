8538a82d616ef91c5ac6f993b575f1c1e331c815
ted <theo_drz@live.com>
2023-04-24 19:55:16 -0400

    added interfaces and more specific types, also created drop downs for each language in AllProjects

--- src/components/AllProjects.tsx
+++ src/components/AllProjects.tsx
@@ -1,54 +1,77 @@
 import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
+interface repo {
+  name: string,
+  language: string,
+  html_url: string,
+  updated_at: string,
+  has_pages: boolean,
+}
+
+interface stateContainer {
+  [key: string]: Array<repo>
+}
+
 function AllProjects() {
-  const [repos, setRepos] = useState([{
-    name: '',
-    language: '',
-    html_url: '',
-    has_pages: false,
-  }]);
+  const [reposV2, setReposeV2] = useState<stateContainer>({})
 
   useEffect(() => {
     fetch('https://api.github.com/users/tediferjones/repos?&per_page=100')
         .then(res => res.json())
         .then(data => {
-          let languages: { [key: string]: Array<string> } = {};
-          const allLanguages = [...new Set(data.map((item: { language: string }) => item.language))];
-          console.log(allLanguages)
-          allLanguages.map((item: any) => {
-            // console.log(languages);
-            // console.log(item)
-            languages[item] = data.filter((dataItem: { language: string }) => dataItem.language === item)
+          let languages: stateContainer = {};
+          // const allLanguages = [...new Set<string>(data.map((item: repo) => item.language))];
+          [...new Set<string>(data.map((item: repo) => item.language))].forEach((item: string) => {
+            languages[item] = data.filter((dataItem: repo) => dataItem.language === item)
           })
-          console.log(languages);
-          // console.log(data.filter((item: any) => item.language === 'TypeScript'))
-          // console.log(data.filter((item: any) => item.language === 'JavaScript'))
-          // console.log(data.filter((item: any) => item.language === 'Ruby'))
-          // console.log(data.filter((item: any) => ['HTML', 'CSS'].includes(item.language)))
 
-          setRepos(data.filter((item: { language: string }) => item.language !== null));
+          // Merge HTML and CSS into one key/value pair
+          languages['HTML/CSS'] = languages.HTML.concat(languages.CSS);
+          delete languages.HTML;
+          delete languages.CSS;
+
+          setReposeV2(languages);
         })
   }, []);
+
+  function getTotal() {
+    // try using reduce instead
+    let total = 0;
+    Object.keys(reposV2).forEach((item: string) => total = total + reposV2[item].length)
+    return total;
+  }
+
+  function toggleRepoList(e: any) {
+    document.getElementById(e.target.value)?.classList.toggle('hidden');
+  }
 
   // This should return a series of dropdowns by language
   //    - For Example: have languages JS, Python, Ruby, have each drop down show its projects
   // State needs to be some kind of dynamic object
+  // To get the length of all arrays from our object, we need to loop through each of its keys, get array.length and add it some accumalator or something like that
 
   return (
     <div>
-      <h1>All Projects ({repos.length})</h1>
-      {repos.map(item => {
+      <h1>All Projects ({getTotal()})</h1>
+      {Object.keys(reposV2).filter((item: string) => item !== 'null').map((item: string) => {
         return (
-          <div className='flex justify-around'
-            key={uuidv4()}
-          >
-            <h3>{item.name}</h3>
-            <h3>{item.language}</h3>
-            <a href={item.html_url}>CODE LINK</a>
-            {item.has_pages ? 
-              <a href={`https://tediferjones.github.io/${item.name}`}>PAGE LINK</a>
-              : []}
+          <div key={uuidv4()}>
+            <button className='text-2xl'
+              onClick={toggleRepoList}
+              value={`${item}RepoList`}
+            >{item} ({reposV2[item].length})</button>
+            <div className='hidden'
+              id={`${item}RepoList`}
+            >
+              {reposV2[item].map((langData: repo) => {
+                return (
+                  <div key={uuidv4()}>
+                    <h1>{langData.name}</h1>
+                  </div>
+                )
+              })}
+            </div>
           </div>
         )
       })}
--- src/components/FavProjects.tsx
+++ src/components/FavProjects.tsx
@@ -1,50 +1,46 @@
 import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
+interface repo {
+  name: string,
+  language: string,
+  html_url: string,
+  updated_at: string,
+  has_pages: boolean,
+}
+
 function FavProjects() {
-  const [recentRepos, setRecentRepos] = useState([{
-    name: '',
-    language: '',
-    html_url: '',
-    has_pages: false,
-  }])
+  const [recentRepos, setRecentRepos] = useState<Array<repo>>([])
 
   useEffect(() => {
     fetch('https://api.github.com/users/tediferjones/repos?&per_page=100')
         .then(res => res.json())
         .then(data => {
           // console.log(data)
-          setRecentRepos(data.sort((a: any, b: any) => {
+          setRecentRepos(data.sort((a: repo, b: repo) => {
             return new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime();
-          }).filter((item: any) => item.has_pages).slice(0, 6));
+          }).filter((item: repo) => item.has_pages).slice(0, 6));
         })
   }, []);
-
-  // fetch('https://www.github.com/tediferjones', {mode: 'no-cors'}).then((data: any) => console.log(data.json()))
 
   // This page should return a series of cards for recent projects
 
   return (
     <div>
       <h1>Recent Projects</h1>
-      {recentRepos.map((item: any) => {
-        // console.log(item);
+      {recentRepos.map((item: repo) => {
         return (
           <div key={uuidv4()}>
             <h3>{item.name}</h3>
             <h4>{item.language}</h4>
             <a href={item.html_url}>CODE LINK</a>
-            {/* 
-            {item.has_pages ? 
-              <a href={`https://tediferjones.github.io/${item.name}`}>PAGE LINK</a>
-              : []}
-            */}
-            {/* only generate iframe if item has the has_pages property */}
-            {item.has_pages ?
-              <>
-                <a href={`https://tediferjones.github.io/${item.name}`}>PAGE LINK</a>
-                <iframe src={`https://tediferjones.github.io/${item.name}`} width='512' height='512'></iframe>
-              </> : []}
+            <a href={`https://tediferjones.github.io/${item.name}`}>PAGE LINK</a>
+            <iframe
+              src={`https://tediferjones.github.io/${item.name}`}
+              title={item.name}
+              width='512'
+              height='512'
+            ></iframe>
           </div>
         )
       })}
