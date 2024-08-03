6015e0401f383b56dd18cc6f182f674bba1af2fe
ted <theo_drz@live.com>
2024-08-02 23:44:06 -0400

    merged noReact branch and improved build process, also added web server to dev environment

--- v2/src/build.ts
+++
@@ -1,8 +0,0 @@
-await Bun.build({
-  entrypoints: [ 'src/index.ts' ],
-  outdir: 'public/',
-  minify: true,
-  splitting: true,
-})
-
-Bun.spawnSync('bunx tailwindcss -i src/globals.css -o public/styles.css'.split(' '))
